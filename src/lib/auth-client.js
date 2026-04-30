c19d25ce3cf76bc2dd2fdc5b3d7cfea0d13e9143
Nazmul-vs-code <khalekbiton1977@gmail.com>
2026-04-30 12:28:02 +0600

    80% done

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
    
     
-    baseURL: "http://localhost:3000"
+    baseURL: "https://assignment-8-lime.vercel.app"
 })
 
 export const { signIn, signUp, signOut, useSession } = authClient;
